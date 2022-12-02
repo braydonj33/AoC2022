6eee8a3fd33d535378187572126ce967d908c368
braydonj33 <119642502+braydonj33@users.noreply.github.com>
2022-12-01 21:11:41 -0600

    Update Day1Part2.java

--- Day1Part2.java
+++ Day1Part2.java
@@ -1,1 +1,46 @@
+import java.util.Scanner;
 
+public class calsNum {
+   public static void main(String[] args) {
+      Scanner stdin = new Scanner(System.in);
+      double mostNum = 0;
+      double secondNum = 0;
+      double thirdNum = 0;
+      double totalNum = 0;
+      double currentNum = 0;
+      while (stdin.hasNextLine()) {
+         String next = stdin.nextLine();
+         if (next.equals("")) {
+            if (currentNum > mostNum) {
+               thirdNum = secondNum;
+               secondNum = mostNum;
+               mostNum = currentNum;
+            } else if (currentNum > secondNum) {
+               thirdNum = secondNum;
+               secondNum = currentNum;
+            } else if (currentNum > thirdNum) {
+               thirdNum = currentNum;
+            }
+            currentNum = 0;
+         } else {
+            currentNum += Double.parseDouble(next);
+         }
+      }
+      if (currentNum != 0) {
+         if (currentNum > mostNum) {
+            thirdNum = secondNum;
+            secondNum = mostNum;
+            mostNum = currentNum;
+         } else if (currentNum > secondNum) {
+            thirdNum = secondNum;
+            secondNum = currentNum;
+         } else if (currentNum > thirdNum) {
+            thirdNum = currentNum;
+         }
+         currentNum = 0;
+      }
+      totalNum = mostNum + secondNum + thirdNum;
+      System.out.println(totalNum);
+      stdin.close();
+   }
+}
