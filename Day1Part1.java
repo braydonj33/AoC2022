4456bfffb60db3b88ddd2a558a19d55c50db1662
braydonj33 <119642502+braydonj33@users.noreply.github.com>
2022-12-01 23:45:30 -0600

    Update Day1Part1.java

--- Day1Part1.java
+++ Day1Part1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class calsNum {
+public class calsNumPart1 {
 	public static void main(String[] args) {
 		Scanner stdin = new Scanner(System.in);
 		double mostNum = 0;
