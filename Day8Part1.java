17e83e2b92422625b7c8e2115b2ccceec75e14c4
braydonj33 <119642502+braydonj33@users.noreply.github.com>
2022-12-08 00:08:11 -0600

    Update Day8Part1.java

--- Day8Part1.java
+++ Day8Part1.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-
 public class calsNum {
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner stdin = new Scanner(System.in);
@@ -24,33 +23,8 @@ public class calsNum {
 		         visTotal++;
 		   }
 		}
-		//for (int i = 1; i < trees[0].length() - 1; i++) {
-		//   visTotal += visibleVertical(trees, i, cnt);
-		//}
 		System.out.println(visTotal);
 	}
-	
-   /*public static int visibleVertical(String[] trees, int num, int lineCnt) {
-	   int visCnt = 0;
-	   boolean vis = true;
-	   boolean vis2 = true;
-	   for (int i = 1; i < lineCnt - 1; i++) {
-	      int intNum = Integer.parseInt(String.valueOf(trees[i].charAt(num)));
-	      for (int j = i - 1; j >= 0; j--) {
-	         int num2 = Integer.parseInt(String.valueOf(trees[j].charAt(num)));
-	         if (intNum <= num2)
-	            vis = false;
-	      }
-	      for (int j = i + 1; j < lineCnt; j++) {
-	         int num2 = Integer.parseInt(String.valueOf(trees[j].charAt(num)));
-	         if (intNum <= num2)
-	            vis2 = false;
-	      }
-	      if (vis || vis2)
-	         visCnt++;
-	   }
-	   return visCnt;
-	}*/
 	public static boolean visibleHorizontal(String[] trees, int rowNum, int lineNum) {
 	   boolean vis = true;
 	   boolean vis2 = true;
